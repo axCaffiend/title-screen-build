20e60a9e129fb405ad31176ac7a8926fb84b0bf6
axCaffiend <axfulcher@outlook.com>
2024-04-24 00:54:41 +0100

    Added javascript file, defined constants fro title screen animation.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("script.js running");
+
+const startButton = document.querySelector(".start-button");
+const car = document.querySelector(".car");
+const road = document.querySelector(".road");
+const treesFg = document.querySelector(".trees.fg");
+const treesBg = document.querySelector(".trees-bg");
