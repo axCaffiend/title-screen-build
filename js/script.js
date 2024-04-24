32a100ec935c58337efe53bf7f5744d173a2f74b
axCaffiend <axfulcher@outlook.com>
2024-04-24 08:25:01 +0100

    Added logo animation.

--- js/script.js
+++ js/script.js
@@ -7,10 +7,7 @@ const treesFg = document.querySelector(".trees-fg");
 const treesBg = document.querySelector(".trees-bg");
 const cloudContainer = document.querySelector(".cloud-container")
 const moon = document.querySelector(".moon");
-const logoText = document.querySelector(".logo_text");
-const logoEarL = document.querySelector(".logo_ear-l");
-const logoEarR = document.querySelector(".logo_ear-r");
-const logoTeeth = document.querySelector(".logo_teeth");
+const logo = document.querySelector(".logo-container");
 
 startButton.addEventListener("click", startGame)
 
@@ -19,16 +16,17 @@ function startGame() {
     // Start button flash and fade out
     startButton.classList.add("start-button-clicked");
 
-    car.classList.add("car-move-off");
-    
     // After car moves off, hide car and stop trees-scroll/zoom out.
+    car.classList.add("car-move-off");
     car.addEventListener("animationend", () => {car.style.display = "none";
     treesBg.classList.add("trees-bg-zoom-out");
     treesFg.classList.add("trees-fg-zoom-out");
     road.classList.add("road-zoom-out");
     moon.classList.add("moon-up");
-    // Logo animation
-    logoText.classList.add("logo_text-reveal");
-    })
+    // console.log(logo);
 
+    logo.classList.add("logo-reveal");
+
+
+    })
 }
